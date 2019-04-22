30e0b056d9a51b425a3a300128ddf74a3c53c73a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-22 18:03:54 +0000

    Update docs after building Travis build 19 of jcrist/yarnspawner
    The docs were built from the branch 'master' against the commit
    d88b61ad1f90ef9c7966cb14da2618b732edf49b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/yarnspawner/jobs/523129080.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0+1.gfb5de0f',
+    VERSION: '0.2.0+2.gd88b61a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
